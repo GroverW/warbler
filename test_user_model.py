f4d691da9420267dfbfe67967cae66c31c51bcf0
GroverW <cheesemaker@gmail.com>
2020-02-06 10:05:35 -0800

    Added tests for following users, creating users and logging in. Updated alert message for taken email / username

--- test_user_model.py
+++ test_user_model.py
@@ -20,7 +20,7 @@ os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 
 # Now we can import app
 
-from app import app, session
+from app import app, session, CURR_USER_KEY
 app.config["DEBUG_TB_HOSTS"] = ["dont-show-debug-toolbar"]
 app.config['WTF_CSRF_ENABLED'] = False
 # Create our tables (we do this here, so we only create the tables
@@ -48,19 +48,18 @@ class UserModelTestCase(TestCase):
             password="HASHED_PASSWORD",
             image_url=""
         )
-        self.u.id = 1
+
         self.u2 = User.signup(
             email="test2@test.com",
             username="testuser2",
             password="HASHED_PASSWORD",
             image_url=""
         )
-        self.u2.id = 2
 
         db.session.add_all([self.u, self.u2])
         db.session.commit()
 
-        self.u = User.query.get(1)
+        self.u1 = User.query.get(1)
         self.u2 = User.query.get(2)
 
         self.client = app.test_client()
@@ -74,43 +73,143 @@ class UserModelTestCase(TestCase):
         """Does basic model work?"""
 
         # User should have no messages & no followers
-        self.assertEqual(len(self.u.messages), 0)
-        self.assertEqual(len(self.u.followers), 0)
+        self.assertEqual(len(self.u1.messages), 0)
+        self.assertEqual(len(self.u1.followers), 0)
 
     def test_user_repr(self):
         """Does the user model repr function work?"""
-    
-        self.assertEqual(str(self.u), f"<User #{self.u.id}: testuser, test@test.com>" )
 
+        self.assertEqual(str(self.u1), f"<User #{self.u1.id}: testuser, test@test.com>" )
 
     def test_user_following(self):
         """ Does the following user feature work?"""
         with self.client as c:
             with c.session_transaction() as sess:
-                sess["curr_user"] = self.u.id
-            
+                sess[CURR_USER_KEY] = self.u1.id
+
             id = self.u2.id
             resp = self.client.post(f"/users/follow/{id}", follow_redirects=True)
 
             html = resp.get_data(as_text=True)
-            
+
             self.assertEqual(resp.status_code, 200)
             self.assertIn("<p>@testuser2</p>", html)
-        
+
         # """Issue was with teardown not resetting the auto incrementing IDs?"""
 
-    
     def test_user_followers(self):
         """ Can other users follow us, and are they displayed?"""
-        #Write later; right now, we have an issue where we can not have one user
-        #access another user? We can only test if they display themselves.
-    
 
-    # def test_user_login(self, username, password):
-    #     """ Can an user log in? """
-    #     resp = self.client.post(f"/login", data = dict(username = self.u2.username, password = self.u2.password),
-    #                                        follow_redirects=True)   
-    #     html = resp.get_data(as_text=True)
-        
-    #     self.assertEqual(resp.status_code, 200)
-    #     self.assertIn("Hello",html)
+        with self.client as c:
+            id = self.u1.id
+            
+            # u2 is not initially following u1
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1.id
+
+            resp = self.client.get(f"/users/{id}/followers")
+
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertNotIn("<p>@testuser2</p>", html)
+
+            # u2 can follow u1
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u2.id
+
+            resp = self.client.post(f"/users/follow/{id}", follow_redirects=True)
+
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+
+            # u2 is following u1
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1.id
+
+            resp = self.client.get(f"/users/{id}/followers")
+
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("<p>@testuser2</p>", html)
+
+    def test_user_login(self):
+        """ Can an user log in? """
+        with self.client as c:
+            resp = self.client.post("/login", data={
+                                        "username": self.u2.username,
+                                        "password": 'HASHED_PASSWORD'
+                                        }, follow_redirects=True)   
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn(f"Hello, {self.u2.username}", html)
+
+    def test_create_user(self):
+        """ Can you create a user? """
+        with self.client as c:
+            resp = self.client.post("/signup", data={
+                                        "username": "testuser3",
+                                        "password": "HASHED_PASSWORD",
+                                        "email": "testuser3@gmail.com",
+                                        "image_url": ""
+                                        }, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("<p>@testuser3</p>", html)
+            self.assertNotIn("Username already taken", html)
+    
+    def test_create_user_fail(self):
+        """ Can you fail to create a user? """
+        with self.client as c:
+            # No email
+            resp = self.client.post("/signup", data={
+                                        "username": "testuser3",
+                                        "password": "HASHED_PASSWORD",
+                                        "email": "",
+                                        "image_url": ""
+                                        }, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<span class="text-danger">This field is required.</span>', html)
+
+            # Bad email
+            resp = self.client.post("/signup", data={
+                                        "username": "testuser3",
+                                        "password": "HASHED_PASSWORD",
+                                        "email": "notanemailaddress",
+                                        "image_url": ""
+                                        }, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<span class="text-danger">Invalid email address.</span>', html)
+
+            # Username already taken
+            resp = self.client.post("/signup", data={
+                                        "username": "testuser",
+                                        "password": "HASHED_PASSWORD",
+                                        "email": "testuser@gmail.com",
+                                        "image_url": ""
+                                        }, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<div class="alert alert-danger">Username / Email already taken</div>', html)
+
+            # Email already taken
+            resp = self.client.post("/signup", data={
+                                        "username": "testuser47",
+                                        "password": "HASHED_PASSWORD",
+                                        "email": "test@test.com",
+                                        "image_url": ""
+                                        }, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<div class="alert alert-danger">Username / Email already taken</div>', html)
+
